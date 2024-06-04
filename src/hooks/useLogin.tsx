29489122a4f41b74fdacb9370f1790f4eaee26bd
Carsten Haupka <carsten.haupka@gmail.com>
2024-06-04 12:27:08 +0200

    add pw to env

--- src/hooks/useLogin.tsx
+++ src/hooks/useLogin.tsx
@@ -4,9 +4,11 @@ import { useState } from 'react';
 export const useLogin = () => {
 
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  console.log(process.env.REACT_APP_PASSWORD);
+  
 
   const checkPassword = (input: string) => {
-    if (input === 'password') {
+    if (input === process.env.REACT_APP_PASSWORD) {
       setIsAuthenticated(true);
     } else {
       alert('Incorrect password!');
